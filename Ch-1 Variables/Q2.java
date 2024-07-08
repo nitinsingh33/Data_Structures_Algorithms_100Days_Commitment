79301d162de664d0154bf55ccbd4cf7898d3ef06
nitinsingh33 <nitinsingh.iitpgmail.com>
2024-07-08 21:59:25 +0530

    solve questions of ch1

---
+++ Ch-1 Variables/Q2.java
@@ -0,0 +1,12 @@
+// In a program, input the side of a square. You have to output the area of the square
+import java.util.*;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int area = a*b;
+        System.out.println("Area of square is: " + area);
+    }
+}
---
+++ Ch-1 Variables/Q3.java
@@ -0,0 +1,16 @@
+/* Enter cost of 3 items from the user (using float data type) - a pencil, a pen and
+an eraser. You have to output the total cost of the items back to the user as their bill.
+(Add on : You can also try adding 18% gst tax to the items in the bill as an advanced problem) */
+
+import java.util.*;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+        float sum = a + b + c;
+        System.out.println("The total cost: " + sum);
+    }
+}
