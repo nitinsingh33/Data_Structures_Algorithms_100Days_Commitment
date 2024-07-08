0a96f307b822b330236bbb21e2cbf4637841045e
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-07-08 22:55:41 +0530

    update

--- Ch-1 Variables/Q3.java
+++ Ch-1 Variables/Q3.java
@@ -9,8 +9,10 @@ public class Q3 {
         Scanner sc = new Scanner(System.in);
         float a = sc.nextFloat();
         float b = sc.nextFloat();
-        float c = sc.nextFloat();
+        
         float sum = a + b + c;
         System.out.println("The total cost: " + sum);
     }
 }
+ 
+//verified commit from windows machine
