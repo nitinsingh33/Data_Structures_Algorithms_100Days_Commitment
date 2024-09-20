486a2027fb3ade61819406b7346494bc1ceb71e3
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-09-20 20:01:28 +0530

    /

--- OOPs/Complex.java
+++ OOPs/Complex.java
@@ -1,6 +1,7 @@
 /*Print the sum, difference and product of two complex numbers by creating a class named 'Complex' with separate methods for each operation whose real and imaginary parts are entered by the user.*/
+
 package OOPs;
 
-public class Q1 {
+public class Complex {
     
 }
