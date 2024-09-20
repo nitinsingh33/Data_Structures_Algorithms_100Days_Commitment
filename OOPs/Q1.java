4f46c65c409ba79b9d92da1a82b2bd3c8e056cc5
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-09-20 19:56:13 +0530

    .

--- OOPs/Q1.java
+++ OOPs/Q1.java
@@ -1,4 +1,4 @@
-/*Print the sum, difference and product of two complex numbers by creating a class named 'Complex' with separate methods  */
+/*Print the sum, difference and product of two complex numbers by creating a class named 'Complex' with separate methods for each operation whose real and imaginary parts are entered by the user. */
 package OOPs;
 
 public class Q1 {
