b7132d1c041c74d89096fa2aae5e62d8dcac10d0
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-09-20 20:20:25 +0530

    .

--- OOPs/Complex.java
+++ OOPs/Complex.java
@@ -1,7 +1,7 @@
 /*Print the sum, difference and product of two complex numbers by creating a class named 'Complex' with separate methods for each operation whose real and imaginary parts are entered by the user.*/
 
 import java.util.*;
-class Complex{
+class Complex {
     int real;
     int imag;
     
@@ -14,8 +14,25 @@ class Complex{
 public static Complex add (Complex a, Complex b) {
     return new Complex ((a.real+b.real), (a.imag+b.imag));
     }
-}
+
 
 public static Complex diff(Complex a, Complex b) {
-    
+    return new Complex((a.real-b.real), (a.imag-b.imag));
+}
+
+public static Complex product(Complex a, Complex b) {
+    return new Complex(((a.real*b.real)-(a.imag*b.imag)),((a.real*b.imag)+(a.imag*b.real)));
+}
+
+public void printComplex () {
+    if(real == 0 && imag!=0) {
+        System.out.println(imag+"i");
+    }
+    else if(imag == 0 && real!=0) {
+        System.out.println(real);
+    }
+    else{
+        System.out.println(real+"+"+imag+"i");
+    }
+}
 }
