d4833652739eb9f11ba8cc0da7c99377ee0be93f
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-12-07 13:33:22 +0530

    .

--- Day-6/Q4.java
+++ Day-6/Q4.java
@@ -1,5 +1,10 @@
-package Day-6;
+//What will be the value of x & y in the following program:
 
 public class Q4 {
+    public static void main(String[] args) {
+        int a = 63, b = 36;
+        boolean x = (a < b ); 
+        int y = (a > b ) ? a : b;
+    }
     
 }
--- Day-6/Q5.java
+++ Day-6/Q5.java
@@ -1,4 +1,4 @@
-package Day-6;
+//Write a java program that takes a year from the user and print whether that year is a leap year or not.
 
 public class Q5 {
     
